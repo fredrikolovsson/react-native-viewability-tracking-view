c680230e678653cee6ab2e104a2df011febf4dae
Fredrik Olovsson <fogithub@gmail.com>
2021-01-31 19:37:32 +0100

    move CreateMeasurementCallbackParameters to types

--- src/ViewabilityTrackingView/createMeasurementCallback/index.ts
+++ src/ViewabilityTrackingView/createMeasurementCallback/index.ts
@@ -1,29 +1,6 @@
 import { Dimensions, MeasureOnSuccessCallback } from 'react-native'
 
-import { ViewabilityTrackingViewProps } from '../types'
-
-interface CreateMeasurementCallbackParameters
-  extends Required<
-      Pick<
-        ViewabilityTrackingViewProps,
-        | 'debug'
-        | 'itemVisiblePercentThreshold'
-        | 'minimumViewTime'
-        | 'offsetBottom'
-        | 'offsetLeft'
-        | 'offsetRight'
-        | 'offsetTop'
-        | 'onViewabilityChange'
-      >
-    >,
-    Pick<ViewabilityTrackingViewProps, 'item' | 'testID'> {
-  hasReportedViewabilityChange: boolean
-  inViewSince: number | null
-  outOfViewSince: number | null
-  setHasReportedViewabilityChange: (bool: boolean) => void
-  setInViewSince: (dateNowOrNull: number | null) => void
-  setOutOfViewSince: (dateNowOrNull: number | null) => void
-}
+import { CreateMeasurementCallbackParameters } from './types'
 
 export const createMeasurementCallback = ({
   debug,
---
+++ src/ViewabilityTrackingView/createMeasurementCallback/types.ts
@@ -0,0 +1,24 @@
+import { ViewabilityTrackingViewProps } from '../types'
+
+export interface CreateMeasurementCallbackParameters
+  extends Required<
+      Pick<
+        ViewabilityTrackingViewProps,
+        | 'debug'
+        | 'itemVisiblePercentThreshold'
+        | 'minimumViewTime'
+        | 'offsetBottom'
+        | 'offsetLeft'
+        | 'offsetRight'
+        | 'offsetTop'
+        | 'onViewabilityChange'
+      >
+    >,
+    Pick<ViewabilityTrackingViewProps, 'item' | 'testID'> {
+  hasReportedViewabilityChange: boolean
+  inViewSince: number | null
+  outOfViewSince: number | null
+  setHasReportedViewabilityChange: (bool: boolean) => void
+  setInViewSince: (dateNowOrNull: number | null) => void
+  setOutOfViewSince: (dateNowOrNull: number | null) => void
+}
